609ee33dd5cf51a15094f8456fc12601ead20b0b
Efraim <goldindoo@gmail.com>
2026-05-21 20:52:50 +0200

    MRI image fills screen

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Cache-first strategy for all GET requests
    ============================================================ */
 
-const CACHE_NAME = 'study-buddy-v110';
+const CACHE_NAME = 'study-buddy-v111';
 
 /* All local files to pre-cache on install */
 const LOCAL_ASSETS = [
