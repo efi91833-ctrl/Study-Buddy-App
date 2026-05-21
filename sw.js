ad7c76287ff739032dc749b6d65c3dfaab3ceade
Efraim <goldindoo@gmail.com>
2026-05-21 21:16:18 +0200

    Add Axial/Coronal/Sagittal view tabs + brain 1 coronal & sagittal

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Cache-first strategy for all GET requests
    ============================================================ */
 
-const CACHE_NAME = 'study-buddy-v116';
+const CACHE_NAME = 'study-buddy-v117';
 
 /* All local files to pre-cache on install */
 const LOCAL_ASSETS = [
